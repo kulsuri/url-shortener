ee8a0c9b3d890cfec682d46514683de4534afc4d
Kulraj Suri <kulraj.suri@gmail.com>
2018-08-31 15:00:45 +0100

    code complete

--- app1.py
+++ app1.py
@@ -20,7 +20,6 @@ def create_sqlite_db(db_file):
     finally:
         conn.close()
 
-# ---------------------------------------------------------------
 # check shortened_urls table has been created in the urls.db database
 def check_table_exists_in_sqlite_db():
     create_table = """
@@ -31,77 +30,32 @@ def check_table_exists_in_sqlite_db():
             """
     try:
         run_query_db2(create_table, None, None, True)
-        # run_query_db(create_table, 'No Fetch')
-        # connect to the database & instantiate a cursor obj
-        # cur = connect_to_db()
-        # cur.execute(create_table)
     except OperationalError:
         pass # pass if table has already been created
-# ---------------------------------------------------------------
-
-
-# ---------------------------------------------------------------
-# connect to the sqlite db: urls.db
-def connect_to_db():
-    conn = sqlite3.connect('urls.db')
-    cursor = conn.cursor()
-    return cursor
-# ---------------------------------------------------------------
-
-# ---------------------------------------------------------------
-#run sql query against the urls.db database
-def run_query_db(query, variable):
-    conn = sqlite3.connect('urls.db')
-    cur = conn.cursor()
-    if variable == None:
-        query_result = cur.execute(query).fetchone()
-    elif variable == 'No Fetch':
-        query_result = cur.execute(query)
-        conn.commit()
-    else:
-        query_result = cur.execute(query, (variable,)).fetchone()
-    conn.close()
-    return query_result
 
 #run sql query against the urls.db database
 def run_query_db2(query, variable, fetch, commit):
-    
     conn = sqlite3.connect('urls.db')
     cur = conn.cursor()
-    
     if variable == None:
         query_result = cur.execute(query)
     elif len(variable) == 1:
         query_result = cur.execute(query, (variable[0],))
     elif len(variable) == 2:
         query_result = cur.execute(query, (variable[0],variable[1]))
-    
     if fetch == 'one':
         query_result = query_result.fetchone()
     elif fetch == 'all':
         query_result = query_result.fetchall()
     elif fetch == None:
         pass
-
     if commit == True:
         conn.commit()
     elif commit == False:
         pass
-
-    # query_result = cur.execute(query, (variable,)).fetchone()
     conn.close()
     return query_result
-# ---------------------------------------------------------------
 
-# def run_query_db(query, variable):
-#     cur = connect_to_db()
-#     # if variable == None:
-#     #     query_result = cur.execute(query).fetchone()
-#     # else:
-#     query_result = cur.execute(query, (*variable,))#.fetchone()
-#     return query_result
-
-# ---------------------------------------------------------------
 # normalize/format urls to a specific format to prevent replication in url_db database
 def url_normalizer(unformatted_url):
     p = urlparse(unformatted_url, 'http')
@@ -116,12 +70,10 @@ def url_normalizer(unformatted_url):
          netloc = netloc[4:].lower()
     p = p._replace(netloc=netloc, path=path)
     return p.geturl()
-# ---------------------------------------------------------------
 
 # check if url already exists in url_db
 def check_url_in_db(normalized_url):
     select_row = "SELECT URL FROM SHORTENED_URLS WHERE URL=?"
-    # query = run_query_db(select_row, normalized_url)
     query = run_query_db2(select_row, [normalized_url], 'one', False)
     if query:
         return True
@@ -131,7 +83,6 @@ def check_url_in_db(normalized_url):
 # get the shortened url from the url_db database
 def get_shortened_url_in_db(normalized_url):
     select_row = "SELECT SHORTENED_URL FROM SHORTENED_URLS WHERE URL=?"
-    # query = run_query_db(select_row, normalized_url)[0]
     query = run_query_db2(select_row, [normalized_url], 'one', False)[0]
     json_response = jsonify({'shortened_url': request.url_root + query})
     return json_response, 201
@@ -139,11 +90,13 @@ def get_shortened_url_in_db(normalized_url):
 # get the full url from the url_db database
 def get_full_url_in_db(shortened_url):
     # decode the shortened url
-    url_key = short_url.decode_url(shortened_url)
+    try:
+        url_key = short_url.decode_url(shortened_url)
+    except:
+        return abort(404)
     # define sql query
     select_row = "SELECT URL FROM SHORTENED_URLS WHERE ID=?"
     # find the url key in the url_db
-    # query = run_query_db(select_row, url_key)
     query = run_query_db2(select_row, [url_key], 'one', False)
     # check if query returns a value
     if query:
@@ -152,66 +105,26 @@ def get_full_url_in_db(shortened_url):
     else:
         return abort(404)
 
-# FUNCTION: CREATE_URL
-# ---------------------------------------------------------------
 # create a new shortened url
 def create_url(normalized_url):
     # define sql query
     select_row = "SELECT MAX(ID) FROM shortened_urls"
     # get the max ID number for url shortening algorithm
     query = run_query_db2(select_row, None, 'one', False)
-    # query = run_query_db(select_row, None)
     max_id = query[0]
     # check url.db is not empty
     if max_id == None:
         max_id = 1
     else:
         max_id += 1
-
+    # store full url and shortened url in variables to be inserted into database
     original_url = normalized_url
     shortened_url = short_url.encode_url(max_id)
-
     # define sql query to insert into db
     insert_row = "INSERT INTO shortened_urls (URL, SHORTENED_URL) VALUES (?, ?)"
-
     # run the query to insert into db
     query = run_query_db2(insert_row, [original_url, shortened_url], None, True)
-
-    # conn = sqlite3.connect('urls.db')
-    # cur = conn.cursor()
-    # cur.execute(insert_row, (original_url, shortened_url))
-    # conn.commit()
-    # conn.close()
-    # cur.execute('INSERT INTO shortened_urls (URL, SHORTENED_URL) VALUES ({ou}, {su})'.format(ou=original_url, su=shortened_url))
-    # cur.execute("INSERT INTO shortened_urls (URL, SHORTENED_URL) VALUES ({ou}, {su})".format(ou='httpgooglecom', su='twat'))
     return get_shortened_url_in_db(original_url)
-
-    # create a dict for the new url to shorten
-    # new_url = {
-    #         'id': max_id,
-    #         'url': normalized_url,
-    #         'shortened_url': short_url.encode_url(max_id)
-    #     }
-
-    # insert_row = "INSERT INTO shortened_urls (URL, SHORTENED_URL) VALUES (?, ?)"
-
-    #run_query_db(insert_row, 'test')
-
-    # list_temp = [new_url['url'], new_url['shortened_url']]
-    # query = run_query_db(insert_row, list_temp)
-    # print(max_id)
-    # print(normalized_url)
-    # print(short_url.encode_url(max_id))
-    ###
-    # cur = connect_to_db()
-    # cur.execute(insert_row, (original_url, shortened_url))
-    # with sqlite3.connect('urls.db') as conn:
-    #     cursor = conn.cursor()
-    #     insert_row = "INSERT INTO shortened_urls (URL, SHORTENED_URL) VALUES (?, ?)"
-    #     cursor.execute(insert_row, ((original_url, shortened_url))
-        # cursor.execute(insert_row, (new_url['url'], new_url['shortened_url']))
-    
-
 
 # define route /
 @app.route("/", methods=['GET'])
@@ -220,58 +133,37 @@ def display_url_db():
     select_all = "SELECT * FROM shortened_urls LIMIT 100"
     # execute query
     query = run_query_db2(select_all, None, 'all', False)
-    # conn = sqlite3.connect('urls.db')
-    # cursor = conn.cursor()
-    # cursor.execute("SELECT * FROM shortened_urls")
-    # db_output = cursor.fetchall()
-    # conn.close()
+    # return the url_db for viewing
+    return jsonify(query), 200
 
-    return jsonify(query), 200 #jsonify(url_db) # return the url_db for viewing
-# ---------------------------------------------------------------
-
-# GET ROUTE /shortened_url
-# ---------------------------------------------------------------
 # define GET route /<shortened_url>
 @app.route('/<string:shortened_url>', methods=['GET'])
 def get_url(shortened_url):
-
     # get the original url for the shortened url
     redirect_link = get_full_url_in_db(shortened_url) 
-
     # redirect to the original url
     return redirect_link, 302
-# ---------------------------------------------------------------
 
-# POST ROUTE
-# ---------------------------------------------------------------
 # define POST route /shorten_url
 @app.route('/shorten_url', methods=['POST'])
 def create_shortened_url():
-
     # store json post request object in variable
     req_data = request.get_json()
-
     # error handling request object - if json post request object is empty or missing url key value pair
     if not req_data or not 'url' in req_data: 
         abort(400)
-
     # normalize/format the url
     normalized_url = url_normalizer(req_data['url'])
-
     # validate the new normalized/formatted url
     if not validators.url(normalized_url):
         abort(400)
-
     # check that the url does not exist in the url_db database
     if check_url_in_db(normalized_url) == True:
         return get_shortened_url_in_db(normalized_url) # return the shortened url from the url_db database
     else:
         # create new shortened url o be added to the url_db database
         return create_url(normalized_url)
-# ---------------------------------------------------------------
 
-# ERROR HANDLERS 
-# ---------------------------------------------------------------
 # define 404 error handler route
 @app.errorhandler(404)
 def not_found(error):
@@ -281,7 +173,6 @@ def not_found(error):
 @app.errorhandler(400)
 def not_found400(error):
     return make_response(jsonify({'error': '400 - failed to decode json object'}), 400)
-# ---------------------------------------------------------------
 
 # this flask app will be run if app.py is run
 if __name__ == '__main__':
